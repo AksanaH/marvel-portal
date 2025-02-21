9abcfeb4a08f0cbdfaf8e06262a088a33120385d
Aksana <aksa.hleb@gmail.com>
2025-02-20 19:29:03 -0600

    Add functionality 'load more' button

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -9,21 +9,49 @@ class CharList extends Component {
     state = {
         charList: [],
         loading: true,
-        error: false
+        error: false,
+        newItemLoading: false,
+        offset: 210,
+        charEnded: false
     }
 
     marvelService = new MarvelService();
 
     componentDidMount() {
-        this.marvelService.getAllCharacters()
+        this.onRequest();
+    }
+
+    onRequest = (offset) => {
+        console.log("Offset before request:", this.state.offset);
+        this.onCharListLoading();
+        this.marvelService.getAllCharacters(offset)
             .then(this.onCharListLoaded)
             .catch(this.onError)
     }
 
-    onCharListLoaded = (charList) => {
+    onCharListLoading = () => {
         this.setState({
-            charList,
-            loading: false
+            newItemLoading: true
+        })
+    }
+
+    onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
+        }
+
+        this.setState(({ offset, charList }) => {
+            const newOffset = offset + 9
+            console.log("New offset after update:", newOffset, newCharList);
+            return {
+                charList: [...charList, ...newCharList],
+                loading: false,
+                newItemLoading: false,
+                offset: newOffset,
+                charEnded: ended
+            }
+
         })
     }
 
@@ -33,7 +61,6 @@ class CharList extends Component {
             loading: false
         })
     }
-
 
     renderItems(arr) {
         const items = arr.map((item) => {
@@ -52,7 +79,7 @@ class CharList extends Component {
                 </li>
             )
         });
-
+        // А эта конструкция вынесена для центровки спиннера/ошибки
         return (
             <ul className="char__grid">
                 {items}
@@ -62,7 +89,7 @@ class CharList extends Component {
 
     render() {
 
-        const { charList, loading, error } = this.state;
+        const { charList, loading, error, offset, newItemLoading, charEnded } = this.state;
 
         const items = this.renderItems(charList);
 
@@ -75,7 +102,11 @@ class CharList extends Component {
                 {errorMessage}
                 {spinner}
                 {content}
-                <button className="button button__main button__long">
+                <button
+                    className="button button__main button__long"
+                    disabled={newItemLoading}
+                    style={{ 'display': charEnded ? 'none' : 'block' }}
+                    onClick={() => this.onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
             </div>
