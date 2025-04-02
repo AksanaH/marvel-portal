384112810675e53b4ca7618995ea5a0464ce4a88
Aksana <aksa.hleb@gmail.com>
2025-04-01 20:22:24 -0500

    Add routes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,11 +1,11 @@
 import { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
 import './charInfo.scss';
-import thor from '../../resources/img/thor.jpeg';
+//import thor from '../../resources/img/thor.jpeg';
 
 class CharInfo extends Component {
 
@@ -119,6 +119,10 @@ const View = ({ char }) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
